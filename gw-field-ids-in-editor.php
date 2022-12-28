6446edb38ed4d8042799519f0235640563eae9c1
David Smith <david@gravitywiz.com>
2022-12-28 11:24:46 -0500

    `gw-field-ids-in-editor.php`: Updated to standardize field indicators. This will now play nice with our new Copy Cat Field Indicators snippet.
    https://github.com/gravitywiz/snippet-library/blob/master/experimental/gpcc-field-indicators.php

--- gw-field-ids-in-editor.php
+++ gw-field-ids-in-editor.php
@@ -13,23 +13,21 @@ add_filter( 'gform_field_content', function( $content, $field ) {
 	if ( ! $_gw_inline_field_id_style ) {
 		$content .= '
 			<style>
-				.gw-inline-field-id {
-				    background-color: #ecedf8;
-				    border: 1px solid #d5d7e9;
-				    border-radius: 40px;
-				    font-size: 0.6875rem;
-				    font-weight: 600;
-				    padding: 0.1125rem 0.4625rem;
-				    margin-bottom: 0.5rem;
-                    display: inline-block;
-                    vertical-align: middle;
+				.gw-field-indicator {
+					margin: 0 0 0 0.6875rem;
+					background-color: #ecedf8;
+					border: 1px solid #d5d7e9;
+					border-radius: 40px;
+					font-size: 0.6875rem;
+					font-weight: 600;
+					padding: 0.1125rem 0.4625rem;
 				}
 			</style>';
 		$_gw_inline_field_id_style = true;
 	}
 
 	$search  = '<\/label>|<\/legend>';
-	$replace = sprintf( '\0 <span class="gw-inline-field-id">ID: %d</span>', $field->id );
+	$replace = sprintf( '\0 <span class="gw-field-indicator gw-inline-field-id">ID: %d</span>', $field->id );
 	$content = preg_replace( "/$search/", $replace, $content, 1 );
 
 	return $content;
